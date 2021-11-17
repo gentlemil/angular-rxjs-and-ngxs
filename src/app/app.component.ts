45e5ab6d0d27fe7629bc9e6db1392d256d5bc7b1
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2021-11-17 02:08:04 +0100

    Use Rxjs Observable to create Http request in country searcher component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap, tap } from 'rxjs/operators';
+import { Country } from './shared/country';
+import { CountryService } from './shared/country.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-http-observables';
+export class AppComponent implements OnInit {
+  public loading = false;
+  public countries$: Observable<Country[]>;
+  private searchTerms = new Subject<string>();
+
+  constructor(private countryService: CountryService) {}
+
+  public search(term: string) {
+    this.searchTerms.next(term);
+  }
+
+  ngOnInit(): void {
+    this.countries$ = this.searchTerms.pipe(
+      tap(_ => this.loading = true),
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.countryService.searchCountry(term)),
+      tap(_ => this.loading = false)
+    )
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from "@angular/common/http";
+import { CountryService } from './shared/country.service';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [CountryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
